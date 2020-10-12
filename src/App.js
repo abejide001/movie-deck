ee1e0e9031b55a77e628a1bd00b57a95fb49bcbb
abejide001 <abejidefemi1@gmail.com>
2020-10-12 11:26:02 +0000

    implement constants #sarsmustend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { FaqsContainer } from "./containers/faqs"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { FooterContainer } from "./containers/footer"
 import { JumbotronContainer } from "./containers/jumbotron"
+import * as ROUTES from './constants/routes'
 
 export default function App() {
   return (
