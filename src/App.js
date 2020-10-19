41829e832a98cfb3831c68519b824414cf536304
abejide001 <abejidefemi1@gmail.com>
2020-10-19 12:05:25 +0000

    add pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react"
-import { FaqsContainer } from "./containers/faqs"
+import { Home, Browse, Signin, Signup } from "./pages"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { FooterContainer } from "./containers/footer"
-import { JumbotronContainer } from "./containers/jumbotron"
+
 import * as ROUTES from './constants/routes'
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer/>
-      <FaqsContainer/>
-      <FooterContainer/>
-    </>
+    <Router>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   )
 }
