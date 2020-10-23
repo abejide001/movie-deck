16b51d602f2d16fa21becb5602c796a9054c3aad
abejide001 <abejidefemi1@gmail.com>
2020-10-23 12:07:24 +0100

    refactor for deployment

---
+++ build/precache-manifest.efb49b887b56594864877f4faa2dd38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7230b95ec78e1b2bdfd3bfee57dbc1fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b6b5b5ab87c213dd36",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f9b6b5b5ab87c213dd36",
+    "url": "/static/js/2.1bea5e07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1bea5e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e213b74aa6eec7c5e10",
+    "url": "/static/js/main.add93e75.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  }
+]);
