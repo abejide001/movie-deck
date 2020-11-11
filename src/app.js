f7141733437dac835040765861f6b9f58dcf16e4
abejide001 <abejidefemi1@gmail.com>
2020-11-11 14:07:18 +0000

    implement user access

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
 import React from "react"
 import { Home, Browse, Signin, Signup } from "./pages"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
+import { BrowserRouter as Router } from "react-router-dom"
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes"
+import { useAuthListener } from "./hooks";
 import * as ROUTES from './constants/routes'
 
 export default function App() {
+  const { user } = useAuthListener()
+  
   return (
     <Router>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
+      <IsUserRedirect
+        user={user}
+        loggeedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}>
         <Signin />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggeedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}>
         <Signup />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect user={user} loggeedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   )
 }
